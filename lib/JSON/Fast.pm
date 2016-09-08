1fedaa3f958f3b426ea43fe1257df9b28451fd17
Timo Paulssen <timonator@perpetuum-immobile.de>
2016-09-08 19:14:09 +0200

    don't asplode when using to-json on a Num.

--- lib/JSON/Fast.pm
+++ lib/JSON/Fast.pm
@@ -12,7 +12,7 @@ sub str-escape(str $text) {
 
 sub to-json($obj is copy, Bool :$pretty = True, Int :$level = 0, Int :$spacing = 2) is export {
     return $obj ?? 'true' !! 'false' if $obj ~~ Bool;
-    return $obj.Str if $obj ~~ Int|Rat;
+    return $obj.Str if $obj ~~ Int|Rat|Num;
     return 'null' if not $obj.defined;
     return "\"{str-escape($obj)}\"" if $obj ~~ Str;
 
